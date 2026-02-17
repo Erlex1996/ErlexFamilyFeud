38e999b7ee090e2b2c0296000e4fd0b70b1c8dae
Erlex1996 <alessiodantimi28@gmail.com>
2026-02-17 10:50:48 +0100

    Aggiunto avatar Divano

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-const avatarLista = ['dino.png','wolf.png','cat.png','spider.png','eagle.png','turtle.png','mouse.png','hat.png','car.png','elephant.png','panda.png'];
+const avatarLista = ['dino.png','wolf.png','cat.png','spider.png','eagle.png','turtle.png','mouse.png','hat.png','car.png','elephant.png','panda.png','couch.jpg'];
 let avatarIndex = {1:0,2:1};
 let avatarSelezionato = {1:null,2:null};
 
